ca3bb8bd74c911b2d138ab5da366111c4e79c682
Maxime <mcote074@uottawa.ca>
2019-11-04 14:12:46 -0500

    Update lec.ml

--- lecture/lec.ml
+++ lecture/lec.ml
@@ -1,5 +1,5 @@
 (*ordre superieur*)
-let rec map f xs =
+let rec map (f:fint->int) (xs:int list) =
   match xs with
   | [] -> []
   | hd::tl -> (f hd)::(map f tl)
@@ -73,4 +73,89 @@ let rec insert (t:tree) (k:key) (v:value) :tree =
   | Node (k', v', left, right) ->  (*cas inductif*)
       if k < k' then Node (k', v', insert left k v, right)
       else if k > k' then Node (k', v',left , insert right k v)
-      else Node (k', v', left, right)                      
+      else Node (k', v', left, right)          
+      ;;            
+
+
+
+
+
+let double (n : int) : int =
+  if n < 0 then raise (Failure "negative input!")
+  else double_nat n
+
+type nat = Zero | Succ of nat
+let rec nat_to_int (n : nat) : int =
+match n with
+Zero -> 0
+| Succ n -> 1 + nat_to_int n
+let rec double_nat (n : nat) : nat =
+match n with
+| Zero -> Zero
+| Succ m -> Succ (Succ (double_nat m))
+
+(*type parametres*)
+type tree2 = (int,string) tree
+
+
+(*Pile fonctionelle*)
+module type INT_STACK =
+  sig
+    type t
+    val empty : unit -> t
+    val push : int -> t -> t
+    val is_empty : t -> bool
+    val pop : t -> t
+    val top : t -> int option
+end
+
+module type STACK =
+  sig
+    type 'a stack
+    val empty : unit -> 'a stack
+    val push : 'a -> 'a stack -> 'a stack
+    val is_empty : 'a stack -> bool
+    val pop : 'a stack -> 'a stack
+    val top : 'a stack -> 'a
+end
+
+module ListStack : STACK =
+  struct
+    type 'a stack = 'a list
+    let empty() : 'a stack = []
+    let push (x:'a)(s:'a stack) : 'a stack = x::s
+    let is_empty(s:'a stack) =
+      match s with
+      | [] -> true
+      | _::_ -> false
+    let pop (s:'a stack) : 'a stack =
+      match s with
+      | [] -> []
+      | _::tl -> tl
+    let top (s:'a stack) :'a option =
+      match s with
+      | [] -> None
+      | h::_ -> Some h
+end
+(*Piles impérative*)
+module type IMP_STACK =
+  sig
+    type ‘a stack
+    val empty : unit -> 'a stack
+    val push : 'a -> 'a stack -> unit
+    val pop : 'a stack -> 'a option
+end
+
+module ImpStack:IMP_STACK =
+  struct
+    type ‘a stack = ('a list) 
+    
+    let empty() : 'a stack = ref []
+
+    let push (x:'a) (s:'a stack) : unit = s := x::(!s)
+
+    let pop (s:'a stack) : 'a option =
+      match !s with
+      | [] -> None
+      | h::t -> (s := t ; Some h)
+end
